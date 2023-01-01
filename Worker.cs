adb16d0a7a706fd6716f0438093f074a81987616
Surendra Chaluvadi <surendra.chaluvadi@gmail.com>
2023-01-01 14:10:25 +0400

    updated to run from service.

--- Worker.cs
+++ Worker.cs
@@ -4,61 +4,40 @@ public class Worker : BackgroundService
     private readonly ILogger<Worker> logger;
     private readonly DirectoryConifg directoryConifg;
     public Worker(ILogger<Worker> logger, DirectoryConifg directoryConifg)
-    {
-        this.logger = logger;
-        this.directoryConifg = directoryConifg;
-    }
+        => (this.logger, this.directoryConifg) = (logger, directoryConifg);
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
             logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            string sourceDirectory = directoryConifg.SourceDirectory;
-            string destinationDirectory = directoryConifg.DestinationDirectory;
-            string filter = directoryConifg.Filter;
             int delayTime = directoryConifg.DelayTime;
-            int threshold = directoryConifg.ThresholdCount;
-            int thresholdTime = directoryConifg.ThreasholdTime;
+            ProcessFiles(directoryConifg.SourceDirectory, directoryConifg.DestinationDirectory);
             await Task.Delay(delayTime, stoppingToken);
-            var fileSystemWatcher = new FileSystemWatcher
-            {
-                Path = directoryConifg.SourceDirectory,
-                IncludeSubdirectories = true,
-                NotifyFilter = NotifyFilters.Attributes |
-                                NotifyFilters.CreationTime |
-                                NotifyFilters.DirectoryName |
-                                NotifyFilters.FileName |
-                                NotifyFilters.LastAccess |
-                                NotifyFilters.LastWrite |
-                                NotifyFilters.Security |
-                                NotifyFilters.Size,
-                Filter = directoryConifg.Filter,
-                EnableRaisingEvents = true
-            };
-            fileSystemWatcher.Changed +=
-                        (source, fileSystemEventArgs) => OnChanged(fileSystemEventArgs);
-            fileSystemWatcher.Created +=
-                        (source, fileSystemEventArgs) => OnChanged(fileSystemEventArgs);
         }
     }
-    public void OnChanged(FileSystemEventArgs fileSystemEventArgs)
+
+    private bool IsOkToProcess()
     {
-        try
+        var timeElapsed = Math.Round((DateTime.Now - Utility.GetProcessInitiatedTime()).TotalMinutes, 0);
+        var processedFilesCount = Utility.GetProcessedFilesCount();
+        var isTimeElapsed = timeElapsed <= directoryConifg.ThreasholdTime;
+        var isThresholdReached = processedFilesCount <= directoryConifg.ThresholdCount;
+        return isTimeElapsed && isThresholdReached;
+    }
+    public void ProcessFiles(string sourceDirectory, string destinationDirectory)
+    {
+        Utility.StartProcess();
+        while (IsOkToProcess())
         {
-            var fileName = fileSystemEventArgs.Name;
-            var sourceFilePath = fileSystemEventArgs.FullPath;
-            var destinationFilePath = Path.Combine(directoryConifg.DestinationDirectory, fileName);
-            logger.LogInformation("{fileName}, with path {sourceFilePath} has been created", fileName, sourceFilePath);
-            Utility.MoveFile(sourceFilePath, destinationFilePath);
-            logger.LogInformation("File moved to {destinationFilePath} at {Now}", destinationFilePath, DateTime.Now);
-        }
-        catch (IOException ioEx)
-        {
-            logger.LogError(ioEx, "{Message} Error occured while processing the file {Name}", ioEx.Message, fileSystemEventArgs.Name);
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "{Message} Error occured while processing the file {Name}", ex.Message, fileSystemEventArgs.Name);
+            var filePath = Utility.GetTopFilePath(sourceDirectory);
+            logger.LogInformation("File Path: {path}", filePath);
+            if (!string.IsNullOrWhiteSpace(filePath))
+            {
+                var sourceFilePath = Path.Combine(sourceDirectory, filePath);
+                var destinationFilePath = Path.Combine(destinationDirectory, filePath);
+                Utility.MoveFile(sourceFilePath, destinationFilePath);
+                Utility.IncrementProcessedFilesCount();
+            }
         }
     }
 }
